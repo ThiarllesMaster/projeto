f8469283e23f431abc308cd936167b15bb0466e8
Thiarlles Gomes <thiarlles@gmail.com>
2019-06-30 11:06:37 +0100

    777

--- src/sample/Example.java
+++ src/sample/Example.java
@@ -3,7 +3,7 @@ package sample;
 public class Example {
 
 	public static String validate(String name) {
-		if (name.equals("Rafinha") || name.equals("Gilbberto")) {
+		if (name.equals("Rafinha") || name.equals("Thiarlles")) {
 			System.out.println("Teste");
 			return "amo";
 		}
