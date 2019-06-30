8b8457b9ab0337799065c558d85ad14b641084d9
Thiarlles Gomes <thiarlles@gmail.com>
2019-06-30 11:24:13 +0100

    Study Merge

--- src/sample/Example.java
+++ src/sample/Example.java
@@ -2,12 +2,6 @@ package sample;
 
 public class Example {
 
-	public static String validate(String name, String references) {
-		if (name.equals("Davi Lucca") ) {
-			System.out.println("Teste");
-			return "amo";
-		}
-		return "";
-	}
+	
 
 }
