92efe04e87d53637e4015fe602008593f1cb999a
Thiarlles Gomes <thiarlles@gmail.com>
2019-06-30 10:34:26 +0100

    Changed file

--- src/sample/Example.java
+++ src/sample/Example.java
@@ -3,7 +3,7 @@ package sample;
 public class Example {
 	
 	public static String validate(String name) {
-		if (name.equals("Rafinha")) {
+		if (name.equals("Rafinha") || name.equals("Greice")) {
 			return "amo";
 		}
 		return "";
