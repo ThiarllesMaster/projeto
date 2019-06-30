3e3ff8323f1389b7605ba6e8db4f3a312e23f4e8
Thiarlles Gomes <thiarlles@gmail.com>
2019-06-30 11:53:04 +0100

    Study merges

--- src/sample/Example.java
+++ src/sample/Example.java
@@ -3,7 +3,6 @@ package sample;
 public class Example {
 
 	public static void method(String name) {
-
 		if (name.equals("Thiarlles")) {
 			String bestFriend;
 			String vc = "Testing in the world";
