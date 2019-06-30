a8588f85a418572a3a8c268fa63479825d8db99e
Thiarlles Gomes <thiarlles@gmail.com>
2019-06-30 10:39:40 +0100

    ...

--- src/sample/Example.java
+++ src/sample/Example.java
@@ -4,6 +4,7 @@ public class Example {
 	
 	public static String validate(String name) {
 		if (name.equals("Rafinha") || name.equals("Greice Souza")) {
+			System.out.println("Teste");
 			return "amo";
 		}
 		return "";
